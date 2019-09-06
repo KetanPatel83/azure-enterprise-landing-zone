6d15afddbfa1fae75d74422b159e211695253c91
fortunecookiezen <phillips.james@gmail.com>
2019-09-06 07:37:52 -0500

    added peering

--- main.tf
+++ main.tf
@@ -70,3 +70,17 @@ resource "azurerm_virtual_network_peering" "spoke-2" {
   virtual_network_name      = "${module.spoke-2.vnet_name}"
   remote_virtual_network_id = "${module.network.vnet_id}"
 }
+
+resource "azurerm_virtual_network_peering" "hub-spoke1" {
+  name                      = "hubtospoke1"
+  resource_group_name       = "${var.resource_group_name}"
+  virtual_network_name      = "${module.network.vnet_name}"
+  remote_virtual_network_id = "${module.spoke-1.vnet_id}"
+}
+
+resource "azurerm_virtual_network_peering" "hub-spoke2" {
+  name                      = "hubtospoke2"
+  resource_group_name       = "${var.resource_group_name}"
+  virtual_network_name      = "${module.network.vnet_name}"
+  remote_virtual_network_id = "${module.spoke-2.vnet_id}"
+}
